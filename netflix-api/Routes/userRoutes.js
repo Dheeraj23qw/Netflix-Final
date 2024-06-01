7e4dec750a72e55427014260c2bc3255d6de2b62
Dheeraj kumar <152294671+Dheeraj23qw@users.noreply.github.com>
2024-06-01 12:55:02 +0530

    Update userRoutes.js

--- netflix-api/Routes/userRoutes.js
+++ netflix-api/Routes/userRoutes.js
@@ -13,8 +13,8 @@ const { auth } = require("../middlewares/auth.js");
 const router = express.Router();
 
 
-router.post("/register", handleregister);
-router.post("/login", handlelogin);
+router.post("register", handleregister);
+router.post("login", handlelogin);
 
 
 module.exports = router;
